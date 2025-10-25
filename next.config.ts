0318d5f1c5d210b8fcd19906f76e0366b4e5b46f
ghcastellano <ghcastellano@gmail.com>
2025-10-25 04:13:06 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "frame-ancestors https://framer.com https://*.framer.app 'self';",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "ALLOW-FROM https://framer.com",
+          },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://framer.com https://*.framer.app *",
+          },
+        ],
+      },
+    ]
+  },
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
