2400874cb726c832db3adcba4a74dd7ea0a3db2e
ghcastellano <ghcastellano@gmail.com>
2025-10-25 19:40:42 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  reactStrictMode: true,
+  // Nada experimental aqui para evitar erros de validação
+};
 export default nextConfig;
