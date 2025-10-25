1bd2cd56ffb19d91826c70a3c091e945a0121346
ghcastellano <ghcastellano@gmail.com>
2025-10-25 19:42:52 -0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,16 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {
+export function middleware(_: NextRequest) {
   const res = NextResponse.next();
-  res.headers.set("X-Frame-Options", "ALLOWALL");
+
+  // CSP permitindo embed. Restrinja se preferir:
+  // "frame-ancestors https://*.framer.app https://*.framer.website https://www.framer.com 'self';"
   res.headers.set("Content-Security-Policy", "frame-ancestors *;");
+
+  // Compatibilidade com navegadores antigos
+  res.headers.set("X-Frame-Options", "ALLOWALL");
+
   return res;
 }
 
