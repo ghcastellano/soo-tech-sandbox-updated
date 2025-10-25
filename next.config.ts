cfa10af757578b2fadad198cb8b651e50ae69392
ghcastellano <ghcastellano@gmail.com>
2025-10-25 06:01:46 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,27 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value:
-              "frame-ancestors 'self' https://sootech.io https://framer.com https://*.framer.app;"
-          },
-          {
-            key: "X-Frame-Options",
-            value: "ALLOW-FROM https://framer.com"
-          },
-          {
-            key: "Referrer-Policy",
-            value: "strict-origin-when-cross-origin"
-          }
-        ]
-      }
-    ]
-  }
-}
+import type { NextConfig } from "next";
 
-module.exports = nextConfig
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    turbo: true
+  }
+};
+
+export default nextConfig;
