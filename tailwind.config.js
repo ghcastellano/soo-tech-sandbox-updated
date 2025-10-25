d3402af3d7ffa1c4d95738be03163be18f6693a8
ghcastellano <ghcastellano@gmail.com>
2025-10-25 19:40:58 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
   theme: {
-    extend: {
-      colors: {
-        brand: {
-          500: "#58CC7A"
-        }
-      }
-    }
+    extend: {}
   },
   plugins: []
 };
